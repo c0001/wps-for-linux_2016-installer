60b784e6430ad47d3e0a911ef9645b86b2aa6427
Entroperiance.J <bmsac0001@gmail.com>
2022-01-23 09:43:46 +0800

    Fatal fix

--- install.sh
+++ install.sh
@@ -11,6 +11,15 @@ done
 this_src_dir="$( cd -P "$( dirname "$this_src" )" >/dev/null && pwd )"
 
 
+for cmd in dirname wc cp unlink ln cat
+do
+    if [[ -z $(command -v $cmd) ]]
+    then
+        echo -e "\e[31mCommand '${cmd}' not found which is used in this script!\e[0m"
+        exit 1
+    fi
+done
+
 type=$1
 
 topdir="${HOME}"/.local
@@ -19,8 +28,37 @@ install -d "$topdir"/lib
 install -d "$topdir"/bin
 install -d "$topdir"/share
 
+uninstall_log="${topdir}/wps_uninstall_$(date -u +"%Y%m%d%H%M%S")_${RANDOM}.txt"
+while [[ -e "${uninstall_log}" ]]
+do
+    uninstall_log="${topdir}/wps_uninstall_$(date -u +"%Y%m%d%H%M%S")_${RANDOM}.txt"
+done
+
+function _mkdir_sub ()
+{
+    local item="$1"
+    if [[ ! -e "$(dirname "${topdir}"/"$item")" ]]
+    then
+        mkdir -p "$(dirname "${topdir}"/"$item")"
+    fi
+}
 
 set -e
+count_install=0
+count_install_const=$(( "$(cat "${this_src_dir}/link.txt" | wc -l )" + "$(cat "${this_src_dir}/file.txt" | wc -l)" ))
+
+function _count_neq_error ()
+{
+    local type=$1
+    if [[ $count_install -ne $count_install_const ]]
+    then
+        echo -e "\e[31m${type}: --- did do count \e[33m${count_install}\e[0m not equal const release of \e[33m${count_install_const}\e[0m !"
+        exit 1
+    else
+        echo -e "\e[32m${type}: --- did do count \e[33m${count_install}\e[0m equal const release of \e[33m${count_install_const}\e[0m !"
+    fi
+}
+
 if [[ $1 == 'install' ]]
 then
 
@@ -28,7 +66,9 @@ then
     do
         if [[ -f "${this_src_dir}"/"$file" ]]
         then
+            _mkdir_sub "$file"
             cp -iv "${this_src_dir}"/"$file" "${topdir}"/"$file"
+            let ++count_install
         fi
     done < "${this_src_dir}"/file.txt
 
@@ -36,10 +76,14 @@ then
     do
         if [[ -f "${this_src_dir}"/"$link" ]]
         then
-            cp -iv "${this_src_dir}"/"$link" "${topdir}"/"$link"
+            _mkdir_sub "$file"
+            # NOTE: use -a option to copy link
+            cp -aiv "${this_src_dir}"/"$link" "${topdir}"/"$link"
+            let ++count_install
         fi
     done < "${this_src_dir}"/link.txt
 
+    _count_neq_error install
 
     # patch desktop icon
     cd "${HOME}/.local/share/applications/"
@@ -63,6 +107,7 @@ then
         if [[ -f "$topdir"/"$f" ]]
         then
             rm -v "$topdir"/"$f"
+            let ++count_install
         fi
     done < "${this_src_dir}"/file.txt
 
@@ -71,8 +116,20 @@ then
         if [[ -h "$topdir"/"$l" ]]
         then
             unlink "$topdir"/"$l"
+            let ++count_install
+        elif [[ -f "$topdir"/"$l" ]]
+        then
+            echo "$topdir"/"$l" >> "$uninstall_log"
         fi
     done < "${this_src_dir}"/link.txt
+
+    if [[ -f "$uninstall_log" ]]
+    then
+        echo -e "\e[31mThere's some non-link file remained! please see log of \"$uninstall_log\""
+        exit 1
+    else
+        _count_neq_error uninstall
+    fi
 else
     echo -e "\e[31mWrong command type $1 (please use install/uninstall)\e[0m"
     exit 1
